9e620eecae9d147c490fee7f78ce45f007a7db9d
Usarneme <jamestcoop@gmail.com>
2024-11-11 10:50:12 -0800

    :tada: adds imports and server setup code to main entrypoint

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Usarneme/receipt-processor/handlers"
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	// TODO: imports mux, handlers, models, etc
+	receiptHandler, err := handlers.NewReceiptHandler()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter()
+
+	// Endpoints
+	router.HandleFunc("/receipts/process", receiptHandler.ProcessReceipt).Methods("POST")
+	router.HandleFunc("/receipts/{id}/points", receiptHandler.GetPoints).Methods("GET")
+
+	fmt.Println("Starting API server on port 8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
 }
