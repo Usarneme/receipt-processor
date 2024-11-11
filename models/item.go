4c08c6c59a13bc3529780d5516e6f1403c12eefc
Usarneme <jamestcoop@gmail.com>
2024-11-11 10:30:59 -0800

    :construction: adds validate method to item model

--- models/item.go
+++ models/item.go
@@ -1,9 +1,20 @@
 package models
 
-// TODO: imports and Validate method
+import (
+	"errors"
+)
 
 type Item struct {
 	ShortDescription string `json:"shortDescription"`
 	Price           string `json:"price"`
 }
 
+func (i *Item) Validate() error {
+	if i.ShortDescription == "" {
+		return errors.New("shortDescription is required")
+	}
+	if i.Price == "" {
+		return errors.New("price is required")
+	}
+	return nil
+}
