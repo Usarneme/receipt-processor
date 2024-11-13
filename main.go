377601dd9e24ec5deb8cf6ccbd8998c7e83ff078
Usarneme <jamestcoop@gmail.com>
2024-11-12 16:31:13 -0800

    :coffin: removes unneccesary endpoint handler in main.go

--- main.go
+++ main.go
@@ -9,27 +9,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-		<html>
-			<head><title>Home Page</title></head>
-			<body>
-				<h1>Welcome to GoLang with Mux Router</h1>
-				<a href="/receipts/process">TODO: Make this a POST form with some data...</a>
-				<a href="receipts/1/points">TODO: Make sure there is a receipt with ID 1 to show data...</a>
-			</body>
-		</html>`
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(html))
-}
-
 func main() {
 	receiptHandler := handlers.NewReceiptHandler()
 	router := mux.NewRouter()
 
 	// Endpoints
-	router.HandleFunc("/", HomeHandler).Methods("GET")
 	router.HandleFunc("/receipts/process", receiptHandler.ProcessReceipt).Methods("POST")
 	router.HandleFunc("/receipts/{id}/points", receiptHandler.GetPoints).Methods("GET")
 
